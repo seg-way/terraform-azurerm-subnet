30dd22c896368a1b7ed7d488701ed2d490f575e3
Ryan Faircloth <ryan@dss-i.com>
2023-08-16 17:42:15 -0400

    fix: vars

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+output "subnet" {
+  value = azurerm_subnet.main.name
+}
 output "virtual_network_name" {
-  value = azurerm_virtual_network.instance.name
+  value = azurerm_subnet.main.virtual_network_name
+}
+output "id" {
+  value = azurerm_subnet.main.id
 }

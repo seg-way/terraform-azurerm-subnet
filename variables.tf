ab54dd4973621bec71369688d782e62d627ccb4e
Ryan Faircloth <ryan@dss-i.com>
2023-08-16 15:27:16 -0400

    feat: intial release

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "resource_group" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+variable "name" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+variable "virtual_network_name" {
+  type = string
+  description = "(optional) describe your variable"
+}
+
+variable "subnet" {
+  type = string
+  description = "(optional) describe your variable"
+}
+
+variable "network" {
+  type        = string
+  description = "(optional) describe your variable"
+}

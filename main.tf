f30ecf584738855a901013039bfd6f067df432da
Ryan Faircloth <ryan@dss-i.com>
2023-08-16 17:56:19 -0400

    feat: enforce_private_link_endpoint_network_policies

--- main.tf
+++ main.tf
@@ -3,4 +3,6 @@ resource "azurerm_subnet" "main" {
   resource_group_name  = var.resource_group
   virtual_network_name = var.virtual_network_name
   address_prefixes     = [var.subnet]
+
+  enforce_private_link_endpoint_network_policies = var.enforce_private_link_endpoint_network_policies
 }
--- variables.tf
+++ variables.tf
@@ -16,3 +16,8 @@ variable "subnet" {
   description = "(optional) describe your variable"
 }
 
+variable "enforce_private_link_endpoint_network_policies" {
+  type = bool
+  default = false
+  description = "(optional) describe your variable"
+}
